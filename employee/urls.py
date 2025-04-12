d4b17bf1019ab33442caee7d5d5662a3670317b5
muhammadderic <muhammad.donny.e@gmail.com>
2025-04-12 14:01:25 +0700

    Create 'create' list page and route, create forms file

--- employee/urls.py
+++ employee/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import employee_list
+from .views import create_employee, employee_list
 
 urlpatterns = [
   path('', employee_list, name="list"),
+  path('create/', create_employee, name='create'),
 ]
--- employee/views.py
+++ employee/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+from .forms import EmployeeForm
 from .models import Employee
+
+def create_employee(request):
+    """
+    View to create new Employee model instance.
+
+    If the request is a POST, validates the form and if valid, saves the form and redirects
+    to the list view. If the request is a GET, renders an empty form.
+
+    :param request: The request object
+    :return: The rendered template
+    """
+    if request.method == 'POST':
+        form = EmployeeForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('list')
+    else:
+        form = EmployeeForm() 
+    return render(request, 'create.html', {'form':form})
 
 def employee_list(request):
     """
