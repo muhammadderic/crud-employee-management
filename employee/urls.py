bc75dd866d224e2a952a5c72c9e156e9c9bab6e2
muhammadderic <muhammad.donny.e@gmail.com>
2025-04-12 13:51:04 +0700

    Create 'show' list of employees page and route

--- employee/urls.py
+++ employee/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import home
+from .views import employee_list
 
 urlpatterns = [
-  path('', home),
+  path('', employee_list, name="list"),
 ]
--- employee/views.py
+++ employee/views.py
@@ -1,5 +1,12 @@
-# from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Employee
 
-def home(request):
-  return HttpResponse("Hello Deric")
+def employee_list(request):
+    """
+    View to list all Employee model instances.
+
+    :param request: The request object
+    :return: The rendered template
+    """
+    employee = Employee.objects.all()
+    return render(request, 'list.html', {'employees':employee})
