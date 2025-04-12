b02b831d44a8c6dce6f88665b9837ed2f96ba69d
muhammadderic <muhammad.donny.e@gmail.com>
2025-04-12 14:06:44 +0700

    Create 'update' list page and route

--- employee/urls.py
+++ employee/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import create_employee, employee_list
+from .views import create_employee, employee_list, update_employee
 
 urlpatterns = [
   path('', employee_list, name="list"),
   path('create/', create_employee, name='create'),
+  path('update/<int:pk>/', update_employee, name="update"),
 ]
--- employee/views.py
+++ employee/views.py
@@ -32,3 +32,26 @@ def employee_list(request):
     """
     employee = Employee.objects.all()
     return render(request, 'list.html', {'employees':employee})
+
+def update_employee(request, pk):
+    """
+    View to update an existing Employee model instance.
+
+    Retrieves the employee instance by primary key (pk). If the request is a POST,
+    validates the form and if valid, updates the employee instance and redirects
+    to the list view. If the request is a GET, renders the form with the current
+    employee details.
+
+    :param request: The request object
+    :param pk: The primary key of the Employee to update
+    :return: The rendered template
+    """
+    employee = Employee.objects.get(id=pk)
+    if request.method == 'POST':
+        form = EmployeeForm(request.POST, instance=employee)
+        if form.is_valid():
+            form.save()
+            return redirect('list')
+    else:
+        form = EmployeeForm(instance=employee)
+    return render(request, 'update.html', {'form':form})
